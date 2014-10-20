a10bca76bbb0c4bc149a1f4bb629fd8b05ac9846
cgeorges <198914devil>
2014-10-20 16:43:03 +0300

    Project constants defined and initialization of bootstrap

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+define('LIBRARY_PATH','library/RestWiz');
+define('APPLICATION_PATH','demo_api');
+
+require_once LIBRARY_PATH . '/Bootstrap/Bootstrap.php';
+
+$bootstrap = new RestWiz_Bootstrap_Bootstrap();
