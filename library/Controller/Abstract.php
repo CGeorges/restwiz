99b6a5389d460fa856d67b2be2f7a676d54397d9
cgeorges <198914devil>
2014-10-20 15:38:07 +0300

    Basic folder structure layout

---
+++ library/Controller/Abstract.php
@@ -0,0 +1,8 @@
+<?php
+class Restwiz_Controller_Abstract {
+
+    public function __construct()
+    {
+
+    }
+}
---
+++ library/View/Abstract_JSON.php
@@ -0,0 +1,8 @@
+<?php
+class Restwiz_View_Abstract_JSON {
+
+    public function __construct()
+    {
+
+    }
+}
