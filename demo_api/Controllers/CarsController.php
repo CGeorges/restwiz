82028bbb9ea46fd823ac8271a1ebf0c7966bb5b1
cgeorges <198914devil>
2014-10-20 18:13:49 +0300

    Comment added in demo resource

--- demo_api/Controllers/CarsController.php
+++ demo_api/Controllers/CarsController.php
@@ -3,6 +3,10 @@ class Cars extends RestWiz_Controller_Abstract {
 
     public function getControllerResource()
     {
+        /*
+         * In a resource you should always return the output and not echo/print/var_dump/etc. This is because
+         * the output will be formatted by the framework in the requested format JSON/XML/more to be added.
+         */
         return $this->getController();
     }
 }
