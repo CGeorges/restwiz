4ac9b87a5842c731abd9c8536fad12df08c33cf6
cgeorges <198914devil>
2014-10-20 17:25:59 +0300

    Controller instance can be accessed inside requests(alias views)

--- library/RestWiz/Controller/Abstract.php
+++ library/RestWiz/Controller/Abstract.php
@@ -1,8 +1,43 @@
 <?php
 class RestWiz_Controller_Abstract {
 
+    private $controller = null;
+
     public function __construct()
     {
 
     }
+
+    /**
+     * Sets current controller instance inside controller so it can be accessed in requests.
+     * @param $controller - Instance of current controller
+     * @return bool
+     */
+    public function setController($controller)
+    {
+        $this->controller = $controller;
+
+        if (!empty($this->controller))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Return current controller instance.
+     * @return bool|RestWiz_Controller_Abstract
+     */
+    public function getController()
+    {
+        if (!empty($this->controller))
+        {
+            return $this->controller;
+        }
+
+        return false;
+    }
+
+
 }
