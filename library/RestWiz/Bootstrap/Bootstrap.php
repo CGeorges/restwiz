c1117de10144ff9ca47f7403ddc55942e0f52af4
cgeorges <198914devil>
2014-10-20 16:42:28 +0300

    Controller setter method created

--- library/RestWiz/Bootstrap/Bootstrap.php
+++ library/RestWiz/Bootstrap/Bootstrap.php
@@ -1,4 +1,19 @@
 <?php
 class RestWiz_Bootstrap_Bootstrap {
 
-} 
+    public $controller = null;
+
+    public function setController($controllerName)
+    {
+        if (!file_exists(APPLICATION_PATH . '/Controllers/' . $controllerName . 'Controller.php'))
+        {
+            throw new Exception($controllerName . 'Controller.php doesn\'t exist!');
+        }
+
+        require_once APPLICATION_PATH . '/Controllers/' . $controllerName . 'Controller.php';
+
+        $this->controller = new $controllerName();
+    }
+
+
+}
