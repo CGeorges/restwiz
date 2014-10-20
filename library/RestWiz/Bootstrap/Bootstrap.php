0c3f59937cea0fe22101f7d5367475aa79752ebb
cgeorges <198914devil>
2014-10-20 16:47:33 +0300

    Added controller getter and comments

--- library/RestWiz/Bootstrap/Bootstrap.php
+++ library/RestWiz/Bootstrap/Bootstrap.php
@@ -1,8 +1,17 @@
 <?php
 class RestWiz_Bootstrap_Bootstrap {
 
+    /**
+     * Action controller
+     * @var null|RestWiz_Controller_Abstract
+     */
     public $controller = null;
 
+    /**
+     * Sets action controller. This is done automatically in Bootstrap.
+     * @param $controllerName
+     * @throws Exception
+     */
     public function setController($controllerName)
     {
         if (!file_exists(APPLICATION_PATH . '/Controllers/' . $controllerName . 'Controller.php'))
@@ -15,5 +24,16 @@ class RestWiz_Bootstrap_Bootstrap {
         $this->controller = new $controllerName();
     }
 
+    /**
+     * Returns current action controller or false if no controller is set
+     * @return false|RestWiz_Controller_Abstract
+     */
+    public function getController()
+    {
+        if (!empty($this->controller))
+            return $this->controller;
+
+        return false;
+    }
 
 }
